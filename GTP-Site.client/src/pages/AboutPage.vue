c1065476e4d3db8f9887831ea7d563b6de9f687c
Liz Madsen <cyberneticdenim@gmail.com>
2021-12-04 14:30:15 -0700

    About Page Updates

--- GTP-Site.client/src/pages/AboutPage.vue
+++ GTP-Site.client/src/pages/AboutPage.vue
@@ -1,6 +1,13 @@
 <template>
-  <div class="about">
-    <h1>This is the about page</h1>
+  <div class="row m-0 justify-content-center">
+    <div class="about card-header col-10 elevation-1">
+      <h1>What is Gaming Theater?</h1>
+      <div class="card-body">
+        Gaming Theater Presents is a brainchild started by Geek Scorpio at the
+        onset of the Covid-19 Pandemic, when working from home became the norm
+        for him and his roommate and friends.
+      </div>
+    </div>
   </div>
 </template>
 
