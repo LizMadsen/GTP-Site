f9e07275a5f93078214457c27442980039a0692e
Liz Madsen <cyberneticdenim@gmail.com>
2021-12-05 23:06:20 -0700

    Updates

--- GTP-Site.client/src/pages/PlaylistsPage.vue
+++ GTP-Site.client/src/pages/PlaylistsPage.vue
@@ -2,31 +2,21 @@
   <div class="row m-0 justify-content-center">
     <div class="col-md-5 elevation-2 mt-5 mx-5">
       <center>
-        <iframe
-          width="560"
-          height="315"
+        <iframe width="560" height="315"
           src="https://www.youtube.com/embed/videoseries?list=PLfNSCHTWj8UPKymDF2tTIZR2Ktx4lg-lM"
-          title="YouTube video player"
-          frameborder="0"
+          title="YouTube video player" frameborder="0"
           allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-          allowfullscreen
-          class="ytPlaylist"
-        ></iframe>
+          allowfullscreen class="ytPlaylist"></iframe>
         <h4>Chrono Trigger</h4>
       </center>
     </div>
     <div class="col-md-5 elevation-2 mt-5">
       <center>
-        <iframe
-          width="560"
-          height="315"
+        <iframe width="560" height="315"
           src="https://www.youtube.com/embed/videoseries?list=PLfNSCHTWj8UNTut_5OA3IxKALcWA6NZ0z"
-          title="YouTube video player"
-          frameborder="0"
+          title="YouTube video player" frameborder="0"
           allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-          allowfullscreen
-          class="ytPlaylist"
-        ></iframe>
+          allowfullscreen class="ytPlaylist"></iframe>
         <h4>Chrono Trigger Outtakes</h4>
       </center>
     </div>
@@ -35,31 +25,21 @@
   <div class="row m-0 justify-content-center">
     <div class="col-md-5 elevation-2 mt-5 mx-5">
       <center>
-        <iframe
-          width="560"
-          height="315"
+        <iframe width="560" height="315"
           src="https://www.youtube.com/embed/videoseries?list=PLfNSCHTWj8UO5ANFeAiiiVDUJPy6Yulhh"
-          title="YouTube video player"
-          frameborder="0"
+          title="YouTube video player" frameborder="0"
           allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-          allowfullscreen
-          class="ytPlaylist"
-        ></iframe>
+          allowfullscreen class="ytPlaylist"></iframe>
         <h4>Legendary Artifacts of Gaming</h4>
       </center>
     </div>
     <div class="col-md-5 elevation-2 mt-5">
       <center>
-        <iframe
-          width="560"
-          height="315"
+        <iframe width="560" height="315"
           src="https://www.youtube.com/embed/videoseries?list=PLfNSCHTWj8UPoqXEx3Uxy-8aigj0UVYM-"
-          title="YouTube video player"
-          frameborder="0"
+          title="YouTube video player" frameborder="0"
           allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-          allowfullscreen
-          class="ytPlaylist"
-        ></iframe>
+          allowfullscreen class="ytPlaylist"></iframe>
         <h4>Game Analysis With Rob</h4>
       </center>
     </div>
@@ -68,31 +48,21 @@
   <div class="row m-0 justify-content-center">
     <div class="col-md-5 elevation-2 mt-5 mx-5">
       <center>
-        <iframe
-          width="560"
-          height="315"
+        <iframe width="560" height="315"
           src="https://www.youtube.com/embed/videoseries?list=PLfNSCHTWj8UPzEOOeN3YE_5h146RUaUrR"
-          title="YouTube video player"
-          frameborder="0"
+          title="YouTube video player" frameborder="0"
           allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-          allowfullscreen
-          class="ytPlaylist"
-        ></iframe>
+          allowfullscreen class="ytPlaylist"></iframe>
         <h4>Super Gouls 'N' Ghosts</h4>
       </center>
     </div>
     <div class="col-md-5 elevation-2 mt-5">
       <center>
-        <iframe
-          width="560"
-          height="315"
+        <iframe width="560" height="315"
           src="https://www.youtube.com/embed/videoseries?list=PLfNSCHTWj8UPVgsB3OQ8IqOHjp-nCLdwi"
-          title="YouTube video player"
-          frameborder="0"
+          title="YouTube video player" frameborder="0"
           allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-          allowfullscreen
-          class="ytPlaylist"
-        ></iframe>
+          allowfullscreen class="ytPlaylist"></iframe>
         <h4>Special Random Videos</h4>
       </center>
     </div>
@@ -100,27 +70,11 @@
 </template>
 
 <script>
-export default {
-  name: 'Playlists'
-}
+  export default {
+    name: 'Playlists'
+  }
 </script>
 
 <style scoped lang="scss">
-.home {
-  display: grid;
-  height: 80vh;
-  place-content: center;
-  text-align: center;
-  user-select: none;
-  .home-card {
-    width: 50vw;
-    > img {
-      height: 200px;
-      max-width: 200px;
-      width: 100%;
-      object-fit: contain;
-      object-position: center;
-    }
-  }
-}
+
 </style>
