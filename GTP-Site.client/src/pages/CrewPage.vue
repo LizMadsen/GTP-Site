f24b2b0de6fa19ac91eba047a5db7ec270a4f3a1
Liz Madsen <cyberneticdenim@gmail.com>
2021-12-05 21:40:08 -0700

    Crew Updates

--- GTP-Site.client/src/pages/CrewPage.vue
+++ GTP-Site.client/src/pages/CrewPage.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="row m-0 mt-5 justify-content-center">
-    <div class="col-2 mt-3 mx-2 card elevation-2">
+    <div class="col-md-2 mt-3 mx-2 card elevation-2">
       <img class="mt-2" src="https://i.imgur.com/klS7Q0U.png" />
-      <b><h5>Geek Scorpio</h5></b>
+      <b><h5>Leo / Geek Scorpio</h5></b>
       <span>
         <a href="https://twitter.com/geek_scorpio"
           ><img class="socialsImg me-1" src="https://i.imgur.com/49GvGxp.png"
@@ -11,53 +11,112 @@
           ><img class="socialsImg" src="https://i.imgur.com/K9cb6IF.png"
         /></a>
       </span>
-      <br />Founder, voice actor, researcher, writer, director, narrator
+      Founder, voice actor, researcher, writer, director, narrator
     </div>
-    <div class="col-2 mt-3 mx-2 card elevation-2">
+    <div class="col-md-2 mt-3 mx-2 card elevation-2">
       <img class="mt-2" src="https://i.imgur.com/G3KqoKt.png" />
       <b><h5>ZombieHand</h5></b>
-      [Socials go here]
-      <br />
+      <span>
+        <a href="https://twitter.com/Zombie_Hand"
+          ><img class="socialsImg me-1" src="https://i.imgur.com/49GvGxp.png"
+        /></a>
+        <a href="https://www.instagram.com/zombiehandcreations/"
+          ><img class="socialsImg me-1" src="https://i.imgur.com/K9cb6IF.png"
+        /></a>
+        <a href="https://www.youtube.com/channel/UCvmB0VqOd1wmz7i_AcTjZkw"
+          ><img class="socialsImg me-1" src="https://i.imgur.com/jaQtj2J.png"
+        /></a>
+        <a href="https://www.twitch.tv/lizombiehand"
+          ><img class="socialsImg me-1" src="https://i.imgur.com/rlv2zLT.png"
+        /></a>
+      </span>
       <p>Artist, voice actor, researcher, writer, narrator</p>
     </div>
-    <div class="col-2 mt-3 mx-2 card elevation-2">
+    <div class="col-md-2 mt-3 mx-2 card elevation-2">
       <img class="mt-2" src="https://i.imgur.com/GSXxNA5.png" />
       <b><h5>DnDaine</h5></b>
-      [Socials go here]
-      <br />
+      <span>
+        <a href="https://twitter.com/DaineNotDane"
+          ><img class="socialsImg me-1" src="https://i.imgur.com/49GvGxp.png"
+        /></a>
+        <a href="https://www.twitch.tv/danddaine"
+          ><img class="socialsImg me-1" src="https://i.imgur.com/rlv2zLT.png"
+        /></a>
+      </span>
       <p>Voice actor, narrator</p>
     </div>
-    <div class="col-2 mt-3 mx-2 card elevation-2">
+    <div class="col-md-2 mt-3 mx-2 card elevation-2">
       <img class="mt-2" src="https://i.imgur.com/i0tx6jv.png" />
       <b><h5>Rob</h5></b>
       [Socials go here]
-      <br />
       <p>Voice actor, researcher, narrator</p>
     </div>
   </div>
   <div class="row m-0 justify-content-center">
-    <div class="col-2 mt-3 mx-2 card elevation-2">
+    <div class="col-md-2 mt-3 mx-2 card elevation-2">
       <img class="mt-2" src="https://i.imgur.com/V9EgFdP.png" />
       <b><h5>Darien</h5></b>
       [Socials go here]
-      <br />
       <p>Voice actor, writer, researcher, narrator</p>
     </div>
-    <div class="col-2 mt-3 mx-2 card elevation-2">
+    <div class="col-md-2 mt-3 mx-2 card elevation-2">
       <img class="mt-2" src="https://i.imgur.com/CzhLJp2.png" />
       <b><h5>Arem the Red</h5></b>
       [Socials go here]
-      <br />
       <p>Voice actor, narrator</p>
     </div>
-    <div class="col-2 mt-3 mx-2 card elevation-2">
+    <div class="col-md-2 mt-3 mx-2 card elevation-2">
       <img class="mt-2" src="https://i.imgur.com/1iA2dT3.png" />
       <b><h5>Moontastical</h5></b>
       [Socials go here]
-      <br />
       <p>Voice actor, narrator</p>
     </div>
-    <div class="col-2 mt-3 mx-2 card elevation-2">CREW PAGE BITCH</div>
+    <div class="col-md-2 mt-3 mx-2 card elevation-2">
+      <img class="mt-2" src="https://i.imgur.com/qJYEIfI.png" />
+      <b><h5>Brandon / TomatoMan</h5></b>
+      <span>
+        <a href="https://twitter.com/TM80MAN"
+          ><img class="socialsImg me-1" src="https://i.imgur.com/49GvGxp.png"
+        /></a>
+        <a href="https://www.youtube.com/c/YieldTomato"
+          ><img class="socialsImg" src="https://i.imgur.com/jaQtj2J.png"
+        /></a>
+      </span>
+      Voice actor, narrator, researcher
+    </div>
+  </div>
+  <div class="row m-0 justify-content-center">
+    <div class="col-md-2 mt-3 mx-2 card elevation-2">
+      <img class="mt-2" src="https://i.imgur.com/V9EgFdP.png" />
+      <b><h5>Darien</h5></b>
+      [Socials go here]
+      <p>Voice actor, writer, researcher, narrator</p>
+    </div>
+    <div class="col-md-2 mt-3 mx-2 card elevation-2">
+      <img class="mt-2" src="https://i.imgur.com/CzhLJp2.png" />
+      <b><h5>Arem the Red</h5></b>
+      [Socials go here]
+      <p>Voice actor, narrator</p>
+    </div>
+    <div class="col-md-2 mt-3 mx-2 card elevation-2">
+      <img class="mt-2" src="https://i.imgur.com/1iA2dT3.png" />
+      <b><h5>Moontastical</h5></b>
+      [Socials go here]
+      <p>Voice actor, narrator</p>
+    </div>
+    <div class="col-md-2 mt-3 mx-2 card elevation-2">
+      <img class="mt-2" src="https://i.imgur.com/qJYEIfI.png" />
+      <b><h5>Brandon / TomatoMan</h5></b>
+      <span>
+        <a href="https://twitter.com/TM80MAN"
+          ><img class="socialsImg me-1" src="https://i.imgur.com/49GvGxp.png"
+        /></a>
+        <a href="https://www.youtube.com/c/YieldTomato"
+          ><img class="socialsImg" src="https://i.imgur.com/jaQtj2J.png"
+        /></a>
+      </span>
+      Voice actor, narrator, researcher
+    </div>
   </div>
 </template>
 
